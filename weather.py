b14ea49ab2eb937ede84129c2a15c3fc6b35e01b
wpted <silverhandsome@gmail.com>
2022-06-05 13:21:12 +0800

    Create Weather Class to check the current weather of your input

--- weather.py
+++ weather.py
@@ -5,30 +5,25 @@ import requests
 
 class Weather:
     def __init__(self, api_key):
-        self.url = "https://api.openweathermap.org/data/3.0/onecall?"
-        self.time = dt.datetime.now()
+        self.url = "https://api.openweathermap.org/data/2.5/weather"
         self.api_key = api_key
-        self.city = None
-        self.lat = 25.06
-        self.lon = 125.56
 
-    def weather(self):
-        parameter = {
-            "appid": self.api_key,
-            "lat": self.lat,
-            "lon": self.lon
-        }
+    def get_weather(self, city):
+        url = f"{self.url}?q={city}&appid={self.api_key}"
+        try:
+            result = requests.get(url=url)
+            result.raise_for_status()
 
-        result = requests.get(url=self.url, params=parameter)
-        result.raise_for_status()
-
-        return result.json()
+            return result.json()
+        except requests.exceptions.RequestException as e:
+            SystemExit(e)
 
 
 def main():
     MY_KEY = os.environ["api_key"]
     w1 = Weather(MY_KEY)
-    print(w1.weather())
+    city_to_check = input("Enter a city name to check the weather:\n")
+    print(w1.get_weather(city_to_check))
 
 
 if __name__ == '__main__':
